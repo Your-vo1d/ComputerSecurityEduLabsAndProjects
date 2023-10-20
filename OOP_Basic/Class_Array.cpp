3b2ab7d33da500ef6345370ce6dfb111343c5bc0
Your_Void <my.vo1d.edge@gmail.com>
2023-10-20 16:00:53 +0300

    Class_Array

--- OOP_Basic/Class_Array.cpp
+++ OOP_Basic/Class_Array.cpp
@@ -4,167 +4,205 @@
 #include <algorithm>
 #include <limits>
 
-class Array { 
+class Array
+{
     int *ptr, len;
 
-    public:
-        
-        //Конструктор 1: по параметрам, длине и диапазону значений
-        Array(int len = 1, int t = 1, int min = 0, int max = 20); 
-        
-        // конструктор 2: по массиву
-        Array(int *, int); 
-        
-        //Конструктор копирования
-        Array(Array &);
-        
-        //Деструктор
-        ~Array();
-        
-        //Перегрузка оператора "=" (присвоение)
-        Array& operator = (Array &);
+public:
+    // Конструктор 1: по параметрам, длине и диапазону значений
+    Array(size_t len = 1, int order = 1, int min = 0, int max = 20);
 
-        //Перегрузка оператор [] (работа с элементами массива по индексу)
-        int &operator [](int);
+    // конструктор 2: по массиву
+    Array(int *, size_t);
 
-        //Проверка на упорядоченность по неубыванию
-        bool Test();
+    // Конструктор копирования
+    Array(Array &);
 
-        // равенство элементов массивов (но не порядка)
-        bool operator == (Array); 
-        
-        //Сортировка Шелла
-        void Shell_sort();
-        void Heapsort();
-        void Hoar_sort();
-        void Bit_sort();
+    // Деструктор
+    ~Array();
 
-        //Перегрузка операции потокового ввода
-        friend std::istream & operator >> (std::istream &, Array &);
-        //Перегрузка операции потокового вывода
-        friend std::ostream & operator << (std::ostream &, Array &);
+    // Перегрузка оператора "=" (присвоение)
+    Array &operator=(Array &);
+
+    // Перегрузка оператор [] (работа с элементами массива по индексу)
+    int &operator[](int);
+
+    // Проверка на упорядоченность по неубыванию
+    bool Test();
+
+    // равенство элементов массивов (но не порядка)
+    bool operator==(Array);
+
+    void Shell_sort(); // Сортировка Шелла
+    void Heapsort();   // Пирамидальная сортировка
+    void Hoar_sort();  // Быстрая сортировка
+    void Bit_sort();   // Битовая сортировка
+
+    // Перегрузка операции потокового ввода
+    friend std::istream &operator>>(std::istream &, Array &);
+    // Перегрузка операции потокового вывода
+    friend std::ostream &operator<<(std::ostream &, Array &);
 };
 
-//Конструктор 1 по параметрам, длине и диапазону значений
-Array::Array(int len, int t, int min, int max) {
-    if (len > 0) {
-        if (min > max) {
+// Конструктор 1 по параметрам, длине и диапазону значений
+Array::Array(size_t len, int order, int min, int max)
+{
+    if (len > 0) // Проверка корректности ввода длины массива
+    {
+        if (min > max) // Проверка на корректность ввода границ диапазона чисел
+        {
             min = min ^ max;
             max = min ^ max;
-            min = min ^ max; 
+            min = min ^ max;
         }
-        t %= 4;
+        // Выделение памяти под массив
         ptr = new int[len];
-        this->len = len;
-        srand(time(NULL));
-        //Если параметр = 1, то будет псевдослучайная последовательность
-        if (t == 1) {
-            for (int i = 0; i < len; i++) {
-                ptr[i] =  min + rand() % (max - min + 1);
-            }
-        }
-        //Если параметр = 2, то будет последовательность формируется по неубыванию
-        else if (t == 2) {
-            for (int i = 0; i < len; i++) {
-                if (i == 0)
-                    ptr[i] = min + rand() % max;
-                else {
-                    ptr[i] = ptr[i - 1] + rand() % max;
+        if (ptr != nullptr) // Проверка выделения памяти
+        {
+            this->len = len;
+            srand(time(NULL)); // Инициализация генератора псевдослучайных чисел
+            if (order == 1)    // Если параметр = 1, то будет псевдослучайная последовательность
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    ptr[i] = min + rand() % (max - min + 1); // Генерация случайного числа в диапазоне
                 }
             }
-        }
-        //Если параметр = 3, то будет последовательность формируется по невозрастанию
-        else if (t == 3) {
-            for (int i = 0; i < len; i++) {
-                if (i == 0)
-                    ptr[i] = max - rand() % min;
-                else {
-                    ptr[i] = ptr[i - 1] - rand() % min;
+            else if (order == 2) // Если параметр = 2, то будет последовательность формируется по неубыванию
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    if (i == 0)
+                        ptr[i] = min + rand() % max;
+                    else
+                    {
+                        ptr[i] = ptr[i - 1] + rand() % max;
+                    }
                 }
             }
+            else if (order == 3) // Если параметр = 3, то будет последовательность формируется по невозрастанию
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    if (i == 0)
+                        ptr[i] = max - rand() % min;
+                    else
+                    {
+                        ptr[i] = ptr[i - 1] - rand() % min;
+                    }
+                }
+            }
+            else // Если параметр другой, то последовательность состоит из 1 элемента (минимального элемента)
+            {
+                std::cout << "Input error.\n";
+                for (int i = 0; i < len; i++)
+                    ptr[i] = min;
+            }
         }
-        //Если параметр другой, то последовательность состоит из 1 элемента (минимального элемента)
-        else {
-            std::cout << "Input error.\n";
+    }
+}
+
+// Конструтор 2 (по указателю и длине массива)
+Array::Array(int *array, size_t len)
+{
+    if (array != nullptr)
+    {
+        ptr = new int[len]; // Выделение памяти для массива
+        if (ptr != nullptr) // Проверка выделение памяти
+        {
+            this->len = len; // Инициализация длины массива
             for (int i = 0; i < len; i++)
-                ptr[i] = min;
+                ptr[i] = array[i]; // Копирование значений массива
         }
     }
 }
 
-//Конструтор 2 (по указателю и длине массива)
-Array::Array(int *array, int len) {
-    ptr = new int[len];
-    this->len = len;
-    for (int i = 0; i < len; i++)
-        ptr[i] = array[i];
-}
-
-//Конструктор копирования 
-Array::Array(Array &a) {
-    len = a.len;
-    ptr = new int[len];
-    for (int i = 0; i < len; i++)
-        ptr[i] = a[i];
-}
-
-//Деструктор
-Array::~Array() {
-    if (ptr) {
-        delete [] ptr;
-        ptr = NULL;
+// Конструктор копирования
+Array::Array(Array &a)
+{
+    len = a.len;        // Инициализация длины массива
+    ptr = new int[len]; // Выделение памяти для массива
+    if (ptr != nullptr) // Проверка выделение памяти
+    {
+        for (int i = 0; i < len; i++)
+            ptr[i] = a[i]; // Копирование значений массива
     }
 }
 
-//Перегрузка операции присвоение
-Array &Array::operator=(Array &a) {
-    if (this != &a) {
-        delete [] ptr;
-        len = a.len;
-        ptr = new int[len];
-        for(int i = 0; i < len; i++)
-            ptr[i] = a[i];
+// Деструктор
+Array::~Array()
+{
+    if (ptr) // Проверка выделение памяти
+    {
+        delete[] ptr; // Освобождение памяти для массива
+        ptr = NULL;   // Обнуляем указатель
     }
-    return *this;
 }
 
-//Перегрузка операции взятие элемента по индексе
-int &Array::operator[](int i) {
-    if (i < 0 || i >= len) {
-        std::cout << "\n Индекс за пределами массива";
-        return ptr[0];
+// Перегрузка операции присвоение
+Array &Array::operator=(Array &a)
+{
+    if (this != &a) // Проверка самоприсвоения
+    {
+        delete[] ptr;       // Освобождение памяти для массива
+        len = a.len;        // Изменение длины массива
+        ptr = new int[len]; // Выделение памяти для массива
+        if (ptr)
+        {
+            for (int i = 0; i < len; i++)
+                ptr[i] = a[i]; // Копирование значений массива
+        }
     }
-    return ptr[i];
+    return *this; // Возвращение текущего объекта
 }
 
-//Проверка на упорядоченность по неубыванию
-bool Array:: Test() {
-    for (int i = 0; i < len - 1; i++) {
-        if (ptr[i] <= ptr[i + 1])
-            return false;
+// Перегрузка операции взятие элемента по индексе
+int &Array::operator[](int i)
+{
+    if (i < 0 || i >= len) //Проверка корректности ввода индекса
+    {
+        std::cout << "\n Индекс за пределами массива" << std::endl; //Вывод сообщения об ошибке
+        return ptr[0]; //Возвращение 1 элемента
     }
-    return true;
+    return ptr[i];//Возвращение i-ого элемента
+}
+
+// Проверка на упорядоченность по неубыванию
+bool Array::Test()
+{
+    for (int i = 0; i < len - 1; i++) //Проход по всем элементам
+    {
+        if (ptr[i] > ptr[i + 1]) //Если текущий элемент меньше следуюшего, то массив неупорядочен
+            return false; //Массив неупорядочен
+    }
+    return true; //Массив упорядочен
 }
 
 // Перегрузка для операции '=='
-bool Array::operator==(Array a) {
-    if (len != a.len) {
+bool Array::operator==(Array a)
+{
+    if (len != a.len)
+    {
         return false;
     }
 
-    //Проверка с перемещением с конца на найденое место
+    // Проверка с перемещением с конца на найденое место
     bool flag = false;
 
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len; j++) {
-            if (a[i] == this->ptr[j]) { 
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            if (a[i] == this->ptr[j])
+            {
                 a[i] = a[len - 1];
-                a.len --;
+                a.len--;
             }
         }
     }
 
-    if (a.len == 1) {
+    if (a.len == 1)
+    {
         return true;
     }
     return false;
@@ -179,20 +217,24 @@ bool Array::operator==(Array a) {
 
     bool found = false;
 
-    for (int i = 0; i < len; i++) {
-        if (arr1[i] != arr2[i]) {
+    for (int i = 0; i < len; i++)
+    {
+        if (arr1[i] != arr2[i])
+        {
             return false;
         }
     }
 }
 
-std::istream& operator>>(std::istream& in, Array& a) {
+std::istream &operator>>(std::istream &in, Array &a)
+{
     size_t len;
 
     std::cout << "Enter the size of the array: ";
     in >> len;
 
-    while (in.fail() || len <= 0) {
+    while (in.fail() || len <= 0)
+    {
         std::cerr << "Invalid input for array size. Please enter a positive integer: ";
         in.clear();
         in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -201,11 +243,13 @@ std::istream& operator>>(std::istream& in, Array& a) {
 
     a.len = len;
 
-    for (size_t i = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i)
+    {
         std::cout << "Enter element " << i << ": ";
         in >> a[i];
 
-        while (in.fail()) {
+        while (in.fail())
+        {
             std::cerr << "Invalid input for element " << i << ". Please enter a valid value: ";
             in.clear();
             in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -216,18 +260,19 @@ std::istream& operator>>(std::istream& in, Array& a) {
     return in;
 }
 
-std::ostream & operator << (std::ostream &out, Array &a) {
+std::ostream &operator<<(std::ostream &out, Array &a)
+{
     for (int i = 0; i < a.len; i++)
         std::cout << a.ptr[i];
     std::cout << std::endl;
 }
 
-
-void main() {
+void main()
+{
     int len1;
     std::cout << "Enter a size: ";
     std::cin >> len1;
-    //Создание массива с параметром - длиной массива
+    // Создание массива с параметром - длиной массива
     Array array1[len1];
     std::cout << array1 << std::endl;
 
