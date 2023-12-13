9a7b48a24aa43c818b86e4cf9505f5fd5ebbea73
Your_Void <my.vo1d.edge@gmail.com>
2023-12-13 21:04:29 +0300

    Реализация метода построения дерева Хаффмана.

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ class HuffmanNode:
     def __lt__(self, other):
         # Метод сравнения для сортировки узлов в куче
         return self.frequency < other.frequency
+
+
+def build_huffman_tree(text):
+    # Построение дерева Хаффмана на основе частот символов в тексте
+    frequency_map = {}
+    for char in text:
+        frequency_map[char] = frequency_map.get(char, 0) + 1
+
+    # Инициализация кучи с узлами Хаффмана
+    heap = [HuffmanNode(char, freq) for char, freq in frequency_map.items()]
+    heapq.heapify(heap)
+
+    # Слияние узлов до построения полного дерева
+    while len(heap) > 1:
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+        merged = HuffmanNode(None, left.frequency + right.frequency)
+        merged.left, merged.right = left, right
+        heapq.heappush(heap, merged)
+
+    return heap[0]
