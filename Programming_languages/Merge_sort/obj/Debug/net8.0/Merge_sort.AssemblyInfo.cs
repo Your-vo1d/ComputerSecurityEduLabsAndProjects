f176bd6cbec3ab88f1b2485db4ff8df4f1da8b24
Your-vo1d <my.vo1d.edge@gmail.com>
2024-02-21 12:20:07 +0300

    comment

--- Programming_languages/Merge_sort/obj/Debug/net8.0/Merge_sort.AssemblyInfo.cs
+++ Programming_languages/Merge_sort/obj/Debug/net8.0/Merge_sort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Merge_sort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff5fe3d6b8e70819e06ee8a4178c8e5320f6841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89652adaaec66c4faa01834592820549b9190138")]
 [assembly: System.Reflection.AssemblyProductAttribute("Merge_sort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Merge_sort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
