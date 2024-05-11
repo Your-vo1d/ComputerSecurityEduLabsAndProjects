62c82c859997d214b0fe29adaaf88ff48ce417c0
Your-vo1d <my.vo1d.edge@gmail.com>
2024-05-11 13:16:31 +0700

    Refactor findNode method in BinarySearchTree and constructor

--- OOP_Basic/Двоичное дерево/BinarySearchTree.h
+++ OOP_Basic/Двоичное дерево/BinarySearchTree.h
@@ -7,12 +7,13 @@
 class BinarySearchTree : public BinaryTree
 {
 public:
+    BinarySearchTree() : BinaryTree() {}
     BinarySearchTree(int n);
     virtual int min();
     virtual int max();
     virtual BinaryTree::Node *addNode(Node* root, int key);
     virtual Node *removeNode(Node* root, int key);
-    Node *findNode(int key);
+    Node *findNode(Node* root, int key);
     Node *minNode(Node* root);
 };
 
