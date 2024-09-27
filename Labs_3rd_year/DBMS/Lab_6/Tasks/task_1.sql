2a9e297c4c695b0b0e1fdcc2bb86f67b26c1baea
Your-vo1d <my.vo1d.edge@gmail.com>
2024-09-27 17:53:33 +0300

    feat(DBMS): add a solution for laboratory work №7 task №1

---
+++ Labs_3rd_year/DBMS/Lab_6/Tasks/task_1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users(
+user_id integer PRIMARY KEY,
+username character varying (40) NOT NULL,
+email charachet(40),
+subscription_id integer,
+date_of_birth date,
+registration_date timestamp
+)
