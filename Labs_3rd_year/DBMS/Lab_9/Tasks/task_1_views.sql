d76caa238a2f5fb37d13911b75c0df9c5f2341f7
Your-vo1d <my.vo1d.edge@gmail.com>
2024-12-09 20:27:17 +0300

    Add table to solution for laboratory work №9 task 1_views

--- Labs_3rd_year/DBMS/Lab_9/Tasks/task_1_views.sql
+++ Labs_3rd_year/DBMS/Lab_9/Tasks/task_1_views.sql
@@ -12,3 +12,20 @@ JOIN movies m ON r.movie_id = m.movie_id        -- Соединение отзы
 JOIN movies_genres mg ON m.movie_id = mg.movie_id -- Соединение фильмов с их жанрами
 JOIN genres g ON mg.genre_id = g.genre_id       -- Соединение для получения названия жанра
 ORDER BY u.user_id, r.review_date DESC;         -- Сортировка по пользователю и дате отзыва
+
+
+CREATE VIEW user_subscription_watchlist AS
+SELECT 
+    u.user_id, 
+    u.user_name AS user_nickname,                 
+    s.description AS subscription_type,          
+    m.title AS movie_title,                      
+    w.added_date AS date_added_to_watchlist      
+FROM project_cinema.users u
+LEFT JOIN project_cinema.subscriptions s 
+    ON u.subscription_id = s.subscription_id      
+LEFT JOIN project_cinema.watchlists w 
+    ON u.user_id = w.user_id                     
+LEFT JOIN project_cinema.movies m 
+    ON w.movie_id = m.movie_id                   
+ORDER BY u.user_id, w.added_date DESC;           
