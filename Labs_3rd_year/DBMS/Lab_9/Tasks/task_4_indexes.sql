593742194c2043be2cfe90e1f4316c31d2fdcbc3
Your-vo1d <my.vo1d.edge@gmail.com>
2024-12-09 19:29:40 +0300

    Add solution for laboratory work №9 task 4_indexes

---
+++ Labs_3rd_year/DBMS/Lab_9/Tasks/task_4_indexes.sql
@@ -0,0 +1,5 @@
+DROP INDEX project_cinema.idx_unique_nickname ;
+DROP INDEX project_cinema.idx_unique_title;
+DROP INDEX project_cinema.idx_movies_rating;
+DROP INDEX project_cinema.idx_watchlist_user_movie;
+DROP INDEX project_cinema.idx_watchlist_user_id;
